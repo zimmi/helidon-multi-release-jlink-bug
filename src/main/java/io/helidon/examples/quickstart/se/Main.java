4bff9fa7a05e58b7902c758214f24442e3b3af3f
thomas <zimmi@users.noreply.github.com>
2020-12-19 01:29:01 +0100

    init

---
+++ src/main/java/io/helidon/examples/quickstart/se/Main.java
@@ -0,0 +1,11 @@
+
+package io.helidon.examples.quickstart.se;
+
+// Run mvn clean install on this application to see the issue
+public final class Main {
+
+    public static void main(final String[] args)  {
+        System.out.println("Hello World!");
+    }
+
+}
